237b96251bc4b304b53c52423f20c17c9ce2e97f
Yordan <yordan10_pv@hotmail.com>
2025-02-14 10:44:52 -0600

    Se ha creado el modelo Ticket

---
+++ Models/Ticket.cs
@@ -0,0 +1,11 @@
+﻿namespace Sistema_de_Compra_de_Tiquetes.Models
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+
+    }
+
+}
